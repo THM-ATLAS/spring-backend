4873a92de7e3a9377121ca7086dfc0baada93d81
Nina Hammer <nina.hammer@mni.thm.de>
2022-04-26 17:08:23 +0200

    init

---
+++ src/main/kotlin/com/example/atlasbackend/AtlasBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.atlasbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtlasBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<AtlasBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/atlasbackend/AtlasBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.atlasbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AtlasBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
