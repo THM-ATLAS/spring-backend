79f799ac3d3fde71e0bd961846bdf4b11954f1b1
Nina Hammer <nina.hammer@mni.thm.de>
2022-04-27 15:26:34 +0200

    finish test get

--- src/main/kotlin/com/example/atlasbackend/controller/taskController.kt
+++ src/main/kotlin/com/example/atlasbackend/controller/taskController.kt
@@ -2,12 +2,13 @@ package com.example.atlasbackend.controller
 
 import com.example.atlasbackend.classes.Task
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TaskController {
-    @GetMapping("/tasks")
+    @GetMapping("/")
     fun getTasks(): Task {
-        return Task("Hier werden die Tasks angezeigt")
+        return Task("test")
     }
 }
