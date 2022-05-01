7832f2ff52b612ef45a3a2a4466f09c5242703e4
Lucas <lucasweissmann@gmx.de>
2022-05-01 16:17:57 +0200

    Added Path Variables to receive User- & Task ID and changed Output

--- src/main/kotlin/com/example/atlasbackend/controller/taskController.kt
+++ src/main/kotlin/com/example/atlasbackend/controller/taskController.kt
@@ -3,21 +3,20 @@ package com.example.atlasbackend.controller
 import com.example.atlasbackend.classes.Task
 import com.example.atlasbackend.service.TaskService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 //@RequestMapping(path = ["/tasks"])
 class TaskController(val taskService: TaskService) {
 
-    // How to reach Task ID & User ID?
-
-    @GetMapping("/tasks/user") // TODO: add /User_ID
-    fun loadTasks(/* userID: String */): Array<Task?> {
-        return taskService.loadTasks()
+    @GetMapping("/tasks/user/{USER_ID}")
+    fun loadTasks(@PathVariable USER_ID: String): Array<Task?> {
+        return taskService.loadTasks(USER_ID)
     }
 
-    @GetMapping("/tasks") // TODO: add /Task_ID
-    fun getTask(/*taskID: Int*/): Task {
-        return taskService.getTask()
+    @GetMapping("/tasks/{TASK_ID}")
+    fun getTask(@PathVariable TASK_ID: Int): Task {
+        return taskService.getTask(TASK_ID)
     }
 }
--- src/main/kotlin/com/example/atlasbackend/service/taskService.kt
+++ src/main/kotlin/com/example/atlasbackend/service/taskService.kt
@@ -2,13 +2,14 @@ package com.example.atlasbackend.service
 
 import com.example.atlasbackend.classes.Task
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PathVariable
 
 @Service
 class TaskService {
 
     // Load Task Overview
     // TODO: Load tasks based on User ID (Currently loading every single task)
-    fun loadTasks(/* userID: String */): Array<Task?> {
+    fun loadTasks(@PathVariable USER_ID: String): Array<Task?> {
 
         val taskLimit = 16 //Task limit per page
         val taskArray = arrayOfNulls<Task>(taskLimit)
@@ -19,21 +20,21 @@ class TaskService {
             // Fill Array one by one
 
         //Test Values
-        taskArray[0] = Task("Programmierung 1", "Content1", true, "Beschreibung1")
-        taskArray[1] = Task("Programmierung 2", "Content2", true, "Beschreibung2")
+        taskArray[0] = Task("Programmierung 1", "Content1", true, "USER ID TEST: $USER_ID")
+        taskArray[1] = Task("Programmierung 2", "Content2", true, "USER ID TEST2: $USER_ID")
 
         // 200: OK
         return taskArray
     }
 
     // Load a Single Task
-    fun getTask(/*taskID: Int*/): Task {
+    fun getTask(@PathVariable TASK_ID: Int): Task {
 
         // Request Task from Database based on TASK_ID
             // 404: If TASK_ID can't be found
             // 402: If no rights to access task
 
         // 200: OK
-        return Task("test", "test", true, "test")
+        return Task("test", "test", true, "TASK ID TEST: $TASK_ID")
     }
 }
