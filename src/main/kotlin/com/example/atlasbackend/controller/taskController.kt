7e7cd25829cd0f989652dde496a148126fdc7d38
Lucas <lucasweissmann@gmx.de>
2022-05-01 17:31:42 +0200

    Added HTTP response status codes & some cleanup

--- src/main/kotlin/com/example/atlasbackend/controller/taskController.kt
+++ src/main/kotlin/com/example/atlasbackend/controller/taskController.kt
@@ -2,21 +2,23 @@ package com.example.atlasbackend.controller
 
 import com.example.atlasbackend.classes.Task
 import com.example.atlasbackend.service.TaskService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 //@RequestMapping(path = ["/tasks"])
 class TaskController(val taskService: TaskService) {
 
-    @GetMapping("/tasks/user/{USER_ID}")
-    fun loadTasks(@PathVariable USER_ID: String): Array<Task?> {
-        return taskService.loadTasks(USER_ID)
+    @GetMapping("/tasks/user/{userID}")
+    fun loadTasks(@PathVariable userID: String): ResponseEntity<Array<Task?>> {
+        return taskService.loadTasks(userID)
     }
 
-    @GetMapping("/tasks/{TASK_ID}")
-    fun getTask(@PathVariable TASK_ID: Int): Task {
-        return taskService.getTask(TASK_ID)
+    @GetMapping("/tasks/{taskID}")
+    fun getTask(@PathVariable taskID: Int): ResponseEntity<Task> {
+        return taskService.getTask(taskID)
     }
 }
