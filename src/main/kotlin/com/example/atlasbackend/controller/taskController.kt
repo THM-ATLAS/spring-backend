602dd79dff19c2bee2cfe75dc4f51b30fa5bb5c0
Nina Hammer <nina.hammer@mni.thm.de>
2022-05-02 18:39:03 +0200

    skelett für Usermanagement

--- src/main/kotlin/com/example/atlasbackend/controller/taskController.kt
+++ src/main/kotlin/com/example/atlasbackend/controller/taskController.kt
@@ -3,11 +3,8 @@ package com.example.atlasbackend.controller
 import com.example.atlasbackend.classes.Task
 import com.example.atlasbackend.service.TaskService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
---
+++ src/main/kotlin/com/example/atlasbackend/service/userService.kt
@@ -0,0 +1,52 @@
+package com.example.atlasbackend.service
+
+import com.example.atlasbackend.classes.AtlasUser
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+
+@Service
+class UserService {
+    fun getUser(id: String): ResponseEntity<AtlasUser> {
+        //TODO: select auf den user mit der ID id
+        //TODO: falls Datensatz nicht gefunden wird:
+        //    return ResponseEntity("Dataset with ID ${id} not found", HttpStatus.NOT_FOUND)
+        //TODO: falls Berechtigungen fehlen:
+        //    return ResponseEntity("You are not allowed to modify task ${id}", HttpStatus.FORBIDDEN)
+        //TODO: sonstiger Fehler der Datenbank
+        //    return ResponseEntity("Error", HttpStatus.INTERNAL_SERVER_ERROR)
+        return ResponseEntity(AtlasUser(id, arrayOf("test", "test")), HttpStatus.OK)
+    }
+
+    fun editUser(user: AtlasUser): ResponseEntity<String> {
+        val id:String = user.user_id
+        //TODO: update auf den user mit der ID id und allen Werten aus user
+        //TODO: falls Datensatz nicht gefunden wird:
+        //    return ResponseEntity("Dataset with ID ${id} not found", HttpStatus.NOT_FOUND)
+        //TODO: falls Berechtigungen fehlen:
+        //    return ResponseEntity("You are not allowed to modify task ${id}", HttpStatus.FORBIDDEN)
+        //TODO: sonstiger Fehler der Datenbank
+        //    return ResponseEntity("Error", HttpStatus.INTERNAL_SERVER_ERROR)
+        return ResponseEntity("edit successful", HttpStatus.OK)
+    }
+
+    fun delUser(id: String): ResponseEntity<String> {
+        //TODO: delete auf den user mit der ID id
+        //TODO: falls Datensatz nicht gefunden wird:
+        //    return ResponseEntity("Dataset with ID ${id} not found", HttpStatus.NOT_FOUND)
+        //TODO: falls Berechtigungen fehlen:
+        //    return ResponseEntity("You are not allowed to modify task ${id}", HttpStatus.FORBIDDEN)
+        //TODO: sonstiger Fehler der Datenbank
+        //    return ResponseEntity("Error", HttpStatus.INTERNAL_SERVER_ERROR)
+        return ResponseEntity("delete successful", HttpStatus.OK)
+    }
+
+    fun addUser(user: AtlasUser): ResponseEntity<String> {
+        //TODO: insert mit user
+        //TODO: falls Berechtigungen fehlen:
+        //    return ResponseEntity("You are not allowed to modify task ${id}", HttpStatus.FORBIDDEN)
+        //TODO: sonstiger Fehler der Datenbank
+        //    return ResponseEntity("Error", HttpStatus.INTERNAL_SERVER_ERROR)
+        return ResponseEntity("insert successful", HttpStatus.OK)
+    }
+}
