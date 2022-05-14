3d2532ea9aa62e04235774ee91fd429db8d3ac60
Lucas <lucasweissmann@gmx.de>
2022-05-14 23:04:46 +0200

    Added Exception Structure

--- src/main/kotlin/com/example/atlasbackend/exception/Exceptions.kt
+++ src/main/kotlin/com/example/atlasbackend/exception/Exceptions.kt
@@ -1,10 +1,90 @@
 package com.example.atlasbackend.exception
 
-object ExerciseNotFoundException : RuntimeException() {
-    private const val serialVersionUID = 1L
-}
+// TODO: Try to combine similar functions
 
-object InternalServerErrorException : RuntimeException() {
-    private const val serialVersionUID = 1L
-}
 
+
+/***** [4xx] CLIENT ERRORS *****/
+
+
+/** [400] BAD REQUEST **/
+
+// One/multiple requested/posted parameters are empty
+object EmptyParameterException : RuntimeException()
+
+// One/multiple requested/posted parameters are invalid types
+object InvalidParameterTypeException : RuntimeException()
+
+// One/multiple requested/posted parameters are too long/short
+object InvalidParameterLengthException : RuntimeException()
+
+
+/** [401] UNAUTHORIZED **/
+
+// Access Token is expired (Return how to authenticate, error & error description)
+object TokenExpiredException : RuntimeException()
+
+// Access Token is missing (Only return how to authenticate)
+object TokenMissingException : RuntimeException()
+
+
+/** [403] FORBIDDEN **/
+// User doesn't have the right to do this thing (Return needed privileges)
+
+// User not allowed to view this page
+object AccessDeniedException : RuntimeException()
+
+// User is not allowed to delete this exercise
+object NotAllowedToDeleteExerciseException : RuntimeException()
+
+// User is not allowed to delete this module
+object NotAllowedToDeleteModuleException : RuntimeException()
+
+// User is not allowed to delete this user
+object NotAllowedToDeleteUserException : RuntimeException()
+
+// User is not allowed to edit this exercise
+object NotAllowedToEditExerciseException : RuntimeException()
+
+// User is not allowed to edit this module
+object NotAllowedToEditModuleException : RuntimeException()
+
+// User is not allowed to edit this user
+object NotAllowedToEditUserException : RuntimeException()
+
+
+/** [404] NOT FOUND **/
+
+// Accessed Page doesn't exist
+object PageNotFoundException : RuntimeException()
+
+// Exercise ID doesn't exist
+object ExerciseNotFoundException : RuntimeException()
+
+// User ID doesn't exist
+object UserNotFoundException : RuntimeException()
+
+// Module ID doesn't exist
+object ModuleNotFoundException : RuntimeException()
+
+
+
+/***** [5xx] SERVER ERRORS *****/
+
+
+/** [500] INTERNAL SERVER ERROR **/
+
+// Unexpected Server Errors
+object InternalServerErrorException : RuntimeException()
+
+
+/** [500] INTERNAL SERVER ERROR **/
+
+// Method not implemented yet
+object NotImplementedException : RuntimeException()
+
+
+/** [???] UNEXPECTED ERROR **/
+
+// Completely Unexpected
+object UnexceptedError : RuntimeException()
