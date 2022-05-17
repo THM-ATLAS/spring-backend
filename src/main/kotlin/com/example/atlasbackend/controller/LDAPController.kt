475341247e5be218cf73a8dc9eabe27093e54b53
Cryztalzone <40550172+Cryztalzone@users.noreply.github.com>
2022-05-17 23:40:24 +0200

    Removed empty line

--- src/main/kotlin/com/example/atlasbackend/controller/LDAPController.kt
+++ src/main/kotlin/com/example/atlasbackend/controller/LDAPController.kt
@@ -3,7 +3,6 @@ package com.example.atlasbackend.controller
 import com.example.atlasbackend.classes.UserRet
 import com.example.atlasbackend.service.LdapUser
 import com.example.atlasbackend.service.LDAPService
-
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
