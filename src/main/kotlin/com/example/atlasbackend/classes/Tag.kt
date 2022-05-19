296ee7fb067acb059556400e5305fc6e8323ef85
Lucas <lucasweissmann@gmx.de>
2022-05-19 22:34:18 +0200

    Added Tag.kt Skeleton

---
+++ src/main/kotlin/com/example/atlasbackend/classes/Tag.kt
@@ -0,0 +1,7 @@
+package com.example.atlasbackend.classes
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("tag")
+data class Tag(@Id var tag_id: Int, var tag_name: String)
