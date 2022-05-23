ad3c84e419bb8facdbd0ab8271dd3e341b47bb2b
Lucas <lucasweissmann@gmx.de>
2022-05-23 16:03:40 +0200

    Added Exceptions for Submissions and changed Setting Exception

--- src/main/kotlin/com/example/atlasbackend/exception/Exceptions.kt
+++ src/main/kotlin/com/example/atlasbackend/exception/Exceptions.kt
@@ -6,6 +6,7 @@ package com.example.atlasbackend.exception
 /***** [4xx] CLIENT ERRORS *****/
 
 
+
 /** [400] BAD REQUEST **/
 
 // One/multiple requested/posted parameters are empty
@@ -26,8 +27,9 @@ object InvalidModuleIDException : RuntimeException()
 // Invalid Tag ID when creating tag
 object InvalidTagIDException : RuntimeException()
 
-// Invalid Setting ID when assigning setting
-object InvalidSettingIDException : RuntimeException()
+// Invalid Submission ID when creating submission
+object InvalidSubmissionIDException : RuntimeException()
+
 
 /** [401] UNAUTHORIZED **/
 
@@ -53,6 +55,9 @@ object NoPermissionToDeleteModuleException : RuntimeException()
 // User is not allowed to delete this user
 object NoPermissionToDeleteUserException : RuntimeException()
 
+// User is not allowed to delete this submission
+object NoPermissionToDeleteSubmissionException : RuntimeException()
+
 // User is not allowed to edit this exercise
 object NoPermissionToEditExerciseException : RuntimeException()
 
@@ -62,11 +67,17 @@ object NoPermissionToEditModuleException : RuntimeException()
 // User is not allowed to edit this user
 object NoPermissionToEditUserException : RuntimeException()
 
+// User is not allowed to edit this submission
+object NoPermissionToEditSubmissionException : RuntimeException()
+
 // User is not allowed to create/edit/delete tags
 object NoPermissionToModifyTagsException : RuntimeException()
 
 // User is not allowed to assign/remove tags for this exercise
 object NoPermissionToModifyExerciseTagsException : RuntimeException()
+
+// Submission was too late
+object SubmissionAfterDeadlineException : RuntimeException()
 
 
 /** [404] NOT FOUND **/
@@ -89,6 +100,12 @@ object RoleNotFoundException : RuntimeException()
 // Tag ID doesn't exist
 object TagNotFoundException : RuntimeException()
 
+// Submission ID doesn't exist
+object SubmissionNotFoundException : RuntimeException()
+
+// Setting ID doesn't exist
+object SettingNotFoundException : RuntimeException()
+
 
 /** [422] UNPROCESSABLE ENTITY **/
 
@@ -96,7 +113,9 @@ object TagNotFoundException : RuntimeException()
 object UnprocessableEntityException : RuntimeException()
 
 
+
 /***** [5xx] SERVER ERRORS *****/
+
 
 
 /** [500] INTERNAL SERVER ERROR **/
