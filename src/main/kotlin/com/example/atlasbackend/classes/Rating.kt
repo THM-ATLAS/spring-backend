808f8aaa2e92dfdafea3eba483273fd7c49cfd53
Cryztalzone <40550172+Cryztalzone@users.noreply.github.com>
2022-05-27 21:37:50 +0200

    Fixed composite key issue in rating class

--- src/main/kotlin/com/example/atlasbackend/classes/Rating.kt
+++ src/main/kotlin/com/example/atlasbackend/classes/Rating.kt
@@ -2,6 +2,10 @@ package com.example.atlasbackend.classes
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import javax.persistence.IdClass
+
+data class PrimaryKey(var user_id: Int, var exercise_id: Int)
 
 @Table("user_exercise_rating")
-data class Rating(@Id var user_id: Int, @Id var exercise_id: Int, var value: Int)
+@IdClass(PrimaryKey::class)
+data class Rating(@Id var user_id: Int, var exercise_id: Int, var value: Int)
--- src/main/kotlin/com/example/atlasbackend/classes/Token.kt
+++ src/main/kotlin/com/example/atlasbackend/classes/Token.kt
@@ -4,5 +4,5 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.sql.Timestamp
 
-@Table("token")
+@Table("user_token")
 data class Token(@Id var token_id: Int, var token: String, var last_used: Timestamp, var user_id: Int)
