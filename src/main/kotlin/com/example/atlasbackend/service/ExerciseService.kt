c3e324058ba3399e940968517266d9392c5be91b
Lucas <lucasweissmann@gmx.de>
2022-06-06 20:56:16 +0200

    Adjusted ExerciseService.kt to support Exercise Types

--- src/main/kotlin/com/example/atlasbackend/service/ExerciseService.kt
+++ src/main/kotlin/com/example/atlasbackend/service/ExerciseService.kt
@@ -2,22 +2,19 @@ package com.example.atlasbackend.service
 
 import com.example.atlasbackend.classes.Exercise
 import com.example.atlasbackend.classes.ExerciseRet
-import com.example.atlasbackend.classes.Rating
+import com.example.atlasbackend.classes.ExerciseType
 import com.example.atlasbackend.exception.*
-import com.example.atlasbackend.repository.ExerciseRepository
-import com.example.atlasbackend.repository.ModuleRepository
-import com.example.atlasbackend.repository.RatingRepository
-import com.example.atlasbackend.repository.UserRepository
+import com.example.atlasbackend.repository.*
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PathVariable
 
 
 @Service
-class ExerciseService(val ratingRepository: RatingRepository, val exerciseRepository: ExerciseRepository, val moduleRepository: ModuleRepository, val userRepository: UserRepository) {
+class ExerciseService(val ratingRepository: RatingRepository, val exerciseRepository: ExerciseRepository, val moduleRepository: ModuleRepository, val userRepository: UserRepository, val exerciseTypeRepository: ExerciseTypeRepository) {
 
     fun loadExercises(): List<ExerciseRet> {
         val ret = exerciseRepository.findAll().map {  e ->
-            ExerciseRet(e.exercise_id, moduleRepository.findById(e.module_id).get(), e.title, e.content, e.description, e.exercisePublic, ratingRepository.averageExerciseRating(e.exercise_id))
+            ExerciseRet(e.exercise_id, moduleRepository.findById(e.module_id).get(), e.title, e.content, e.description, e.exercisePublic, ratingRepository.averageExerciseRating(e.exercise_id), exerciseTypeRepository.getExerciseTypeName(e.type_id))
         }.toList()
         return ret
     }
@@ -29,7 +26,7 @@ class ExerciseService(val ratingRepository: RatingRepository, val exerciseReposi
         }
 
         val ret = exerciseRepository.getExercisesByUser(userId).map {  e->
-            ExerciseRet(e.exercise_id, moduleRepository.findById(e.module_id).get(), e.title, e.content, e.description, e.exercisePublic, ratingRepository.averageExerciseRating(e.exercise_id))
+            ExerciseRet(e.exercise_id, moduleRepository.findById(e.module_id).get(), e.title, e.content, e.description, e.exercisePublic, ratingRepository.averageExerciseRating(e.exercise_id), exerciseTypeRepository.getExerciseTypeName(e.type_id))
         }.toSet()
 
         return ret
@@ -40,7 +37,7 @@ class ExerciseService(val ratingRepository: RatingRepository, val exerciseReposi
             throw ModuleNotFoundException
         }
         val ret = exerciseRepository.getExercisesByModule(moduleId).map {  e ->
-            ExerciseRet(e.exercise_id, moduleRepository.findById(moduleId).get(), e.title, e.content, e.description, e.exercisePublic, ratingRepository.averageExerciseRating(e.exercise_id))
+            ExerciseRet(e.exercise_id, moduleRepository.findById(moduleId).get(), e.title, e.content, e.description, e.exercisePublic, ratingRepository.averageExerciseRating(e.exercise_id), exerciseTypeRepository.getExerciseTypeName(e.type_id))
         }.toList()
 
         return ret
@@ -56,7 +53,13 @@ class ExerciseService(val ratingRepository: RatingRepository, val exerciseReposi
         // TODO: Falls Berechtigungen fehlen (Wenn Spring Security steht):
         //   throw AccessDeniedException
 
-        return ExerciseRet(exercise.exercise_id, moduleRepository.findById(exercise.module_id).get(), exercise.title, exercise.content, exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exercise.exercise_id))
+        return ExerciseRet(exercise.exercise_id, moduleRepository.findById(exercise.module_id).get(), exercise.title, exercise.content, exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exercise.exercise_id), exerciseTypeRepository.getExerciseTypeName(exercise.type_id))
+    }
+
+    fun getExerciseTypes(): List<ExerciseType> {
+        return exerciseTypeRepository.findAll().map {  et ->
+            ExerciseType(et.type_id, et.name)
+        }.toList()
     }
 
     fun updateExercise(exercise: ExerciseRet): ExerciseRet {
@@ -69,11 +72,11 @@ class ExerciseService(val ratingRepository: RatingRepository, val exerciseReposi
         //    throw NoPermissionToEditExerciseException
 
 
-        val updatedExercise = Exercise(exercise.exercise_id, exercise.module.module_id, exercise.title, exercise.content, exercise.description, exercise.exercisePublic)
+        val updatedExercise = Exercise(exercise.exercise_id, exercise.module.module_id, exerciseTypeRepository.getExerciseTypeID(exercise.type), exercise.title, exercise.content, exercise.description, exercise.exercisePublic)
 
         exerciseRepository.save(updatedExercise)
 
-        return ExerciseRet(exercise.exercise_id, moduleRepository.findById(exercise.module.module_id).get(), exercise.title, exercise.content, exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exercise.exercise_id))
+        return ExerciseRet(exercise.exercise_id, moduleRepository.findById(exercise.module.module_id).get(), exercise.title, exercise.content, exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exercise.exercise_id), exercise.type)
     }
 
     fun createExercise(exercise: Exercise): ExerciseRet {
@@ -90,7 +93,7 @@ class ExerciseService(val ratingRepository: RatingRepository, val exerciseReposi
         //    throw NoPermissionToEditExerciseException
 
         exerciseRepository.save(exercise)
-        return ExerciseRet(exercise.exercise_id, moduleRepository.findById(exercise.module_id).get(), exercise.title, exercise.content, exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exercise.exercise_id))
+        return ExerciseRet(exercise.exercise_id, moduleRepository.findById(exercise.module_id).get(), exercise.title, exercise.content, exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exercise.exercise_id), exerciseTypeRepository.getExerciseTypeName(exercise.type_id))
     }
 
     fun deleteExercise(exerciseID: Int): ExerciseRet {
@@ -107,7 +110,7 @@ class ExerciseService(val ratingRepository: RatingRepository, val exerciseReposi
         // TODO: Falls Berechtigungen fehlen (Wenn Spring Security steht):
         //   throw AccessDeniedException
 
-        val ret = ExerciseRet(exercise.exercise_id, moduleRepository.findById(exercise.module_id).get(), exercise.title, exercise.content, exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exercise.exercise_id))
+        val ret = ExerciseRet(exercise.exercise_id, moduleRepository.findById(exercise.module_id).get(), exercise.title, exercise.content, exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exercise.exercise_id), exerciseTypeRepository.getExerciseTypeName(exercise.type_id))
 
         exerciseRepository.deleteById(exerciseID)
 
