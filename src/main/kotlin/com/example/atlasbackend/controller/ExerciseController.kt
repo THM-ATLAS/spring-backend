5db5f1faaf3981ca38542aec5499a03a9e9661d4
seppel28 <sebastian-gue@web.de>
2022-06-07 22:51:17 +0200

    Added Annotations for the API-Docs

--- src/main/kotlin/com/example/atlasbackend/controller/ExerciseController.kt
+++ src/main/kotlin/com/example/atlasbackend/controller/ExerciseController.kt
@@ -4,46 +4,96 @@ import com.example.atlasbackend.classes.Exercise
 import com.example.atlasbackend.classes.ExerciseRet
 import com.example.atlasbackend.classes.ExerciseType
 import com.example.atlasbackend.service.ExerciseService
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class ExerciseController(val exerciseService: ExerciseService) {
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Returns all Exercises"),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @GetMapping("/exercises")
     fun loadExercises(): List<ExerciseRet> {
         return exerciseService.loadExercises()
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Returns all Exercises of an User"),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "UserNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @GetMapping("/exercises/user/{userID}")
     fun loadExercises(@PathVariable userID: Int): Set<ExerciseRet> {
         return exerciseService.loadExercisesUser(userID)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Returns all Exercises of a Module"),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "ModuleNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @GetMapping("/exercises/module/{modID}")
     fun loadExercisesModule(@PathVariable modID: Int): List<ExerciseRet> {
         return exerciseService.loadExercisesModule(modID)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Returns Exercises with requested ID"),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "ExerciseNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @GetMapping("/exercises/{exerciseID}")
     fun getExercise(@PathVariable exerciseID: Int): ExerciseRet {
         return exerciseService.getExercise(exerciseID)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Returns Types of Exercises "),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+            ])
     @GetMapping("/exercises/types")
     fun getExerciseTypes(): List<ExerciseType> {
         return exerciseService.getExerciseTypes()
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Edits Exercises"),
+                ApiResponse(responseCode = "403", description = "NoPermissionToEditExerciseException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "ExerciseNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @PutMapping("/exercises")
     fun editExercise(@RequestBody body: ExerciseRet): ExerciseRet {
         return exerciseService.updateExercise(body)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Creates Exercises"),
+                ApiResponse(responseCode = "400", description = "InvalidUserIDException - Exercise ID must be 0", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "403", description = "NoPermissionToEditExerciseException", content = [Content(schema = Schema(hidden = true))]),
+            ])
     @PostMapping("/exercises")
     fun postExercise(@RequestBody exercise: Exercise): ExerciseRet {
         return exerciseService.createExercise(exercise)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Deletes Exercises"),
+                ApiResponse(responseCode = "403", description = "NoPermissionToDeleteExerciseException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "ExerciseNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @DeleteMapping("/exercises/{exerciseID}")
     fun deleteExercise(@PathVariable exerciseID: Int): ExerciseRet {
         return exerciseService.deleteExercise(exerciseID)
--- src/main/kotlin/com/example/atlasbackend/controller/UserController.kt
+++ src/main/kotlin/com/example/atlasbackend/controller/UserController.kt
@@ -2,6 +2,10 @@ package com.example.atlasbackend.controller
 
 import com.example.atlasbackend.service.UserService
 import com.example.atlasbackend.classes.UserRet
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,41 +17,89 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class UserController(val userService: UserService) {
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Returns all Users"),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @GetMapping("/users")
     fun getAllUsers(): List<UserRet> {
         return userService.getAllUsers()
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Returns logged in User"),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))])
+
+    ])
     @GetMapping("/users/me")
     fun getMe()/*: UserRet*/ {
         //return userService.getMe()
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Returns User with requested ID"),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "UserNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @GetMapping("/users/{id}")
     fun getUser(@PathVariable id: Int): UserRet {
         return userService.getUser(id)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Edits User "),
+                ApiResponse(responseCode = "400", description = "InvalidRoleIDException - valid IDs 1,2,4,5", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "UserNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @PutMapping("/users")
     fun editUser(@RequestBody body: UserRet): UserRet {
         return userService.editUser(body)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Creates User "),
+                ApiResponse(responseCode = "400", description = "InvalidUserIDException - ID must be 0 || InvalidRoleIDException - valid IDs 1,2,4,5", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @PostMapping("/users")
     fun addUser(@RequestBody body: UserRet): UserRet {
         return userService.addUser(body)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Creates multiple Users "),
+                ApiResponse(responseCode = "400", description = "InvalidUserIDException - User ID must be 0 || InvalidRoleIDException - valid IDs 1,2,4,5", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @PostMapping("/users/multiple")
     fun addUsers(@RequestBody body: List<UserRet>): List<UserRet> {
         return userService.addUsers(body)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Deletes User with Requested ID"),
+                ApiResponse(responseCode = "403", description = "NoPermissionToDeleteUserException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "UserNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @DeleteMapping("/users/{id}")
     fun delUser(@PathVariable id: Int): UserRet {
         return userService.delUser(id)
     }
 
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "OK - Deletes multiple Users"),
+                ApiResponse(responseCode = "403", description = "NoPermissionToDeleteUserException", content = [Content(schema = Schema(hidden = true))]),
+                ApiResponse(responseCode = "404", description = "UserNotFoundException", content = [Content(schema = Schema(hidden = true))])
+            ])
     @DeleteMapping("/users/multiple")
     fun delUsers(@RequestBody body: List<UserRet>): List<UserRet> {
         return userService.delUsers(body)
