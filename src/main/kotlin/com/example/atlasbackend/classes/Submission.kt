5eebe57079b18fd9605d4286c2a6e736510eda2a
Lucas <lucasweissmann@gmx.de>
2022-06-08 23:37:02 +0200

    Implemented Teacher, Submission Timestamp & Comment, also made Grade Nullable

--- src/main/kotlin/com/example/atlasbackend/classes/Submission.kt
+++ src/main/kotlin/com/example/atlasbackend/classes/Submission.kt
@@ -2,6 +2,7 @@ package com.example.atlasbackend.classes
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import java.sql.Timestamp
 
 @Table("user_exercise_submission")
-data class Submission(@Id var submission_id: Int, var exercise_id: Int, var user_id: Int, var file: String, var grade: Int)
+data class Submission(@Id var submission_id: Int, var exercise_id: Int, var user_id: Int, var file: String, var upload_time: Timestamp, var grade: Int?, var teacher_id: Int?, var comment: String?)
