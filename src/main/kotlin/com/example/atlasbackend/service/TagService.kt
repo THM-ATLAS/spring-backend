0d60de5ee43a8233d5ebc35a3e94a61a860df830
seppel28 <sebastian-gue@web.de>
2022-06-10 00:25:25 +0200

    tags eingebaut

--- src/main/kotlin/com/example/atlasbackend/service/TagService.kt
+++ src/main/kotlin/com/example/atlasbackend/service/TagService.kt
@@ -3,38 +3,63 @@ package com.example.atlasbackend.service
 import com.example.atlasbackend.classes.Exercise
 import com.example.atlasbackend.classes.Tag
 import com.example.atlasbackend.exception.*
+import com.example.atlasbackend.repository.ExerciseRepository
 import com.example.atlasbackend.repository.TagRepository
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.*
 
 @Service
-class TagService(val tagRepository: TagRepository) {
+class TagService(val tagRepository: TagRepository, val exerciseRepository: ExerciseRepository) {
 
     fun getAllTags(): List<Tag>{
-        throw NotYetImplementedException
+        return tagRepository.findAll().toList()
     }
 
-    fun loadExerciseTags(@PathVariable exerciseID: Int): List<Tag> {
-        throw NotYetImplementedException
+    fun loadExerciseTags(exerciseID: Int): List<Tag> {
+        if(!exerciseRepository.existsById(exerciseID)){
+            throw ExerciseNotFoundException
+        }
+        return tagRepository.getExerciseTags(exerciseID)
     }
 
-    fun editTag(@RequestBody body: Tag): Tag {
-        throw NotYetImplementedException
+    fun editTag(tag: Tag): Tag {
+        if(!tagRepository.existsById(tag.tag_id)){
+            throw TagNotFoundException
+        }
+        tagRepository.save(tag)
+        return tag
     }
 
-    fun postTag(@RequestBody body: Tag): Tag {
-        throw NotYetImplementedException
+    fun postTag(tag: Tag): Tag {
+        if(tag.tag_id != 0){
+            throw InvalidTagIDException
+        }
+        tagRepository.save(tag)
+        return tag
     }
 
-    fun addExerciseTag(@PathVariable("exerciseID") exerciseID: Int, @PathVariable("tagID") tagID: Int): Exercise {
-        throw NotYetImplementedException
+    fun addExerciseTag(exerciseID: Int, tagID: Int): Exercise {
+        if(!exerciseRepository.existsById(exerciseID)){
+            throw ExerciseNotFoundException
+        }
+        if(!tagRepository.existsById(tagID)){
+            throw TagNotFoundException
+        }
+        tagRepository.addExerciseTag(exerciseID,tagID)
+        return exerciseRepository.findById(exerciseID).get()
     }
 
-    fun deleteTag(@PathVariable tagID: Int): Tag {
-        throw NotYetImplementedException
+    fun deleteTag(tagID: Int): Tag {
+        if(!tagRepository.existsById(tagID)){
+            throw TagNotFoundException
+        }
+        val tag = tagRepository.findById(tagID).get()
+        tagRepository.deleteById(tagID)
+        return tag
     }
 
-    fun deleteExerciseTag(@PathVariable("exerciseID") exerciseID: Int, @PathVariable("tagID") tagID: Int): Exercise {
-        throw NotYetImplementedException
+    fun deleteExerciseTag(exerciseID: Int, tagID: Int): Exercise {
+        tagRepository.removeExerciseTag(exerciseID,tagID)
+        return exerciseRepository.findById(exerciseID).get()
     }
 }
