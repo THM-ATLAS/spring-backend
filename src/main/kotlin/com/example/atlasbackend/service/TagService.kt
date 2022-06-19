23dc8ebeaaf720f598f6b727bfa601e60601df63
lucas <lucasweissmann@gmx.de>
2022-06-19 14:23:27 +0200

    Implemented permission checking for tags

--- src/main/kotlin/com/example/atlasbackend/service/TagService.kt
+++ src/main/kotlin/com/example/atlasbackend/service/TagService.kt
@@ -1,5 +1,6 @@
 package com.example.atlasbackend.service
 
+import com.example.atlasbackend.classes.AtlasUser
 import com.example.atlasbackend.classes.ExerciseRet
 import com.example.atlasbackend.classes.Tag
 import com.example.atlasbackend.exception.*
@@ -7,66 +8,82 @@ import com.example.atlasbackend.repository.*
 import org.springframework.stereotype.Service
 
 @Service
-class TagService(val tagRepository: TagRepository, val exerciseRepository: ExerciseRepository,val moduleRepository: ModuleRepository, val ratingRepository: RatingRepository, val exerciseTypeRepository: ExerciseTypeRepository) {
+class TagService(val tagRep: TagRepository, val exRep: ExerciseRepository, val modRep: ModuleRepository, val ratRep: RatingRepository, val exTyRep: ExerciseTypeRepository) {
 
     fun getAllTags(): List<Tag>{
-        return tagRepository.findAll().toList()
+        return tagRep.findAll().toList()
     }
 
     fun loadExerciseTags(exerciseID: Int): List<Tag> {
 
         // Error Catching
-        if(!exerciseRepository.existsById(exerciseID)) throw ExerciseNotFoundException
+        if(!exRep.existsById(exerciseID)) throw ExerciseNotFoundException
 
-        return tagRepository.getExerciseTags(exerciseID)
+        // Functionality
+        return tagRep.getExerciseTags(exerciseID)
     }
 
-    fun editTag(tag: Tag): Tag {
+    fun editTag(user: AtlasUser, tag: Tag): Tag {
 
         // Error Catching
-        if(!tagRepository.existsById(tag.tag_id)) throw TagNotFoundException
+        if(!tagRep.existsById(tag.tag_id)) throw TagNotFoundException
+        if (!user.roles.any { r -> r.role_id < 3}) throw NoPermissionToModifyTagsException   // Check for admin/teacher
 
-        tagRepository.save(tag)
+        // Functionality
+        tagRep.save(tag)
         return tag
     }
 
-    fun postTag(tag: Tag): Tag {
+    fun postTag(user: AtlasUser, tag: Tag): Tag {
 
         // Error Catching
         if(tag.tag_id != 0) throw InvalidTagIDException
+        if (!user.roles.any { r -> r.role_id < 3}) throw NoPermissionToModifyTagsException   // Check for admin/teacher
 
-        tagRepository.save(tag)
+        // Functionality
+        tagRep.save(tag)
         return tag
     }
 
-    fun addExerciseTag(exerciseID: Int, tagID: Int): ExerciseRet {
+    fun addExerciseTag(user: AtlasUser, exerciseID: Int, tagID: Int): ExerciseRet {
 
         // Error Catching
-        if(!exerciseRepository.existsById(exerciseID)) throw ExerciseNotFoundException
-        if(!tagRepository.existsById(tagID)) throw TagNotFoundException
+        if(!exRep.existsById(exerciseID)) throw ExerciseNotFoundException
+        if(!tagRep.existsById(tagID)) throw TagNotFoundException
+        if (!user.roles.any { r -> r.role_id == 1} &&   // Check for admin
+            modRep.getModuleRoleByUser(user.user_id, exRep.getModuleByExercise(exerciseID).module_id).let { mru -> mru == null || mru.role_id > 3 })   // Check for tutor/teacher
+            throw NoPermissionToModifyExerciseTagsException
 
-        tagRepository.addExerciseTag(exerciseID,tagID)
-        val exercise = exerciseRepository.findById(exerciseID).get()
-        return ExerciseRet(exerciseID, moduleRepository.findById(exercise.module_id).get(), exercise.title,exercise.content,exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exerciseID), exerciseTypeRepository.getExerciseTypeName(exercise.type_id),tagRepository.getExerciseTags(exerciseID))
+        // Functionality
+        tagRep.addExerciseTag(exerciseID,tagID)
+        val exercise = exRep.findById(exerciseID).get()
+        return ExerciseRet(exerciseID, modRep.findById(exercise.module_id).get(), exercise.title,exercise.content,exercise.description, exercise.exercisePublic, ratRep.averageExerciseRating(exerciseID), exTyRep.getExerciseTypeName(exercise.type_id),tagRep.getExerciseTags(exerciseID))
     }
 
-    fun deleteTag(tagID: Int): Tag {
+    fun deleteTag(user: AtlasUser, tagID: Int): Tag {
 
         // Error Catching
-        if(!tagRepository.existsById(tagID)) throw TagNotFoundException
+        if(!tagRep.existsById(tagID)) throw TagNotFoundException
+        if (!user.roles.any { r -> r.role_id < 3}) throw NoPermissionToModifyTagsException   // Check for admin/teacher
 
-        val tag = tagRepository.findById(tagID).get()
-        tagRepository.deleteById(tagID)
+        // Functionality
+        val tag = tagRep.findById(tagID).get()
+        tagRep.deleteById(tagID)
         return tag
     }
 
-    fun deleteExerciseTag(exerciseID: Int, tagID: Int): ExerciseRet {
+    fun deleteExerciseTag(user: AtlasUser, exerciseID: Int, tagID: Int): ExerciseRet {
 
         // Error Catching
-        if(!tagRepository.existsById(tagID)) throw TagNotFoundException
+        if(!exRep.existsById(exerciseID)) throw ExerciseNotFoundException
+        if(!tagRep.existsById(tagID)) throw TagNotFoundException
+        if (!user.roles.any { r -> r.role_id == 1} &&   // Check for admin
+            modRep.getModuleRoleByUser(user.user_id, exRep.getModuleByExercise(exerciseID).module_id).let { mru -> mru == null || mru.role_id > 3 })   // Check for tutor/teacher
+            throw NoPermissionToModifyExerciseTagsException
 
-        tagRepository.removeExerciseTag(exerciseID,tagID)
-        val exercise = exerciseRepository.findById(exerciseID).get()
-        return ExerciseRet(exerciseID, moduleRepository.findById(exercise.module_id).get(), exercise.title,exercise.content,exercise.description, exercise.exercisePublic, ratingRepository.averageExerciseRating(exerciseID), exerciseTypeRepository.getExerciseTypeName(exercise.type_id),tagRepository.getExerciseTags(exerciseID))
+        // Functionality
+        tagRep.removeExerciseTag(exerciseID,tagID)
+        val exercise = exRep.findById(exerciseID).get()
+        return ExerciseRet(exerciseID, modRep.findById(exercise.module_id).get(), exercise.title,exercise.content,exercise.description, exercise.exercisePublic, ratRep.averageExerciseRating(exerciseID), exTyRep.getExerciseTypeName(exercise.type_id),tagRep.getExerciseTags(exerciseID))
     }
 }
