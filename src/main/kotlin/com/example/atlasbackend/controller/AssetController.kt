736951ab51a3e7ea7455af4f45e0be6ef80411e5
Cryztalzone <40550172+Cryztalzone@users.noreply.github.com>
2022-06-22 14:53:02 +0200

    Added API docs

--- src/main/kotlin/com/example/atlasbackend/controller/AssetController.kt
+++ src/main/kotlin/com/example/atlasbackend/controller/AssetController.kt
@@ -3,6 +3,10 @@ package com.example.atlasbackend.controller
 import com.example.atlasbackend.classes.Asset
 import com.example.atlasbackend.classes.AssetBase64
 import com.example.atlasbackend.service.AssetService
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.apache.tomcat.jni.File
 import org.springframework.core.io.Resource
 import org.springframework.http.ResponseEntity
@@ -19,31 +23,68 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/")
 class AssetController(val assetService: AssetService) {
 
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "OK - Returns an Image as PNG"),
+            ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+            ApiResponse(responseCode = "404", description = "AssetNotFoundException", content = [Content(schema = Schema(hidden = true))])
+        ])
     @GetMapping("/assets/{id}/view")
     fun viewAsset(@PathVariable id: Int): ResponseEntity<Resource> {
         return assetService.viewAsset(id)
     }
 
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "OK - Downloads a file"),
+            ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+            ApiResponse(responseCode = "404", description = "AssetNotFoundException", content = [Content(schema = Schema(hidden = true))])
+        ])
     @GetMapping("/assets/{id}/download")
     fun downloadAsset(@PathVariable id: Int): ResponseEntity<Resource> {
         return assetService.downloadAsset(id)
     }
 
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "OK - Returns an Asset as Base64"),
+            ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+            ApiResponse(responseCode = "404", description = "AssetNotFoundException", content = [Content(schema = Schema(hidden = true))])
+        ])
     @GetMapping("/assets/{id}")
     fun getAsset(@PathVariable id: Int): AssetBase64 {
         return assetService.getAsset(id)
     }
 
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "OK - Creates Asset"),
+            ApiResponse(responseCode = "400", description = "InvalidAssetIDException - ID must be 0", content = [Content(schema = Schema(hidden = true))]),
+            ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))])
+        ])
     @PostMapping("/assets/")
     fun addAsset(@RequestBody asset: AssetBase64): AssetBase64 {
         return assetService.addAsset(asset)
     }
 
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "OK - Edits an Asset"),
+            ApiResponse(responseCode = "400", description = "InvalidAssetIDException - valid IDs 1,2,4,5", content = [Content(schema = Schema(hidden = true))]),
+            ApiResponse(responseCode = "403", description = "AccessDeniedException", content = [Content(schema = Schema(hidden = true))]),
+            ApiResponse(responseCode = "404", description = "AssetNotFoundException", content = [Content(schema = Schema(hidden = true))])
+        ])
     @PutMapping("/assets/")
     fun editAsset(@RequestBody asset: AssetBase64): AssetBase64 {
         return assetService.editAsset(asset)
     }
 
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "OK - Deletes Asset with Requested ID"),
+            ApiResponse(responseCode = "403", description = "NoPermissionToDeleteAssetException", content = [Content(schema = Schema(hidden = true))]),
+            ApiResponse(responseCode = "404", description = "AssetNotFoundException", content = [Content(schema = Schema(hidden = true))])
+        ])
     @DeleteMapping("/assets/{id}")
     fun delAsset(@PathVariable id: Int): AssetBase64 {
         return assetService.delAsset(id)
