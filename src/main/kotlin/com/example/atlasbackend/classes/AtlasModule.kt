2149621376dce7debacbef0fb395458b5f0b248e
@hblh66 <hamza.belhaj@mni.thm.de>
2022-06-23 14:03:00 +0200

    adding icons to exercises/modules

--- src/main/kotlin/com/example/atlasbackend/classes/AtlasModule.kt
+++ src/main/kotlin/com/example/atlasbackend/classes/AtlasModule.kt
@@ -4,4 +4,4 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("module")
-data class AtlasModule(@Id var module_id: Int, var name: String, var description: String)
+data class AtlasModule(@Id var module_id: Int, var name: String, var description: String, var icon: String)
--- src/main/kotlin/com/example/atlasbackend/classes/Tag.kt
+++ src/main/kotlin/com/example/atlasbackend/classes/Tag.kt
@@ -4,4 +4,4 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("tag")
-data class Tag(@Id var tag_id: Int, var name: String)
+data class Tag(@Id var tag_id: Int, var name: String, var icon: String)
