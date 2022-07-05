49bdbd011d5938000fbdebc464c5ef30298e2fcc
Cryztalzone <40550172+Cryztalzone@users.noreply.github.com>
2022-07-05 17:10:30 +0200

    Added link classes and repos

---
+++ src/main/kotlin/com/example/atlasbackend/repository/ModuleAssetRef.kt
@@ -0,0 +1,7 @@
+package com.example.atlasbackend.repository
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ModuleAssetRef: CrudRepository<ModuleAssetRef, Int>
---
+++ src/main/kotlin/com/example/atlasbackend/repository/ModuleLinkRef.kt
@@ -0,0 +1,4 @@
+package com.example.atlasbackend.repository
+
+interface ModuleLinkRef {
+}
